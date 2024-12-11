38bcca5aa29b2c06dc3021be8e55c1b75c419ee0
jaisonjeba <jaiprabakar5@gmail.com>
2024-12-11 16:14:02 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Todo from './Todo';
+
+function App(){
+  return(
+    <div className='container'>
+      <Todo />
+    </div>
+  )
+}
+
+export default App;
